a9fa604aa68f3624346511e7dbf4f47bbb3fd75e
JerryLee <leechiyin1990@gmail.com>
2018-05-21 18:04:25 +0930

    21/5

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,6 @@ function mobilenav_close(){
   document.getElementById("sidemenu").style.width = "0";
 }
 
-
 var dropdown = document.getElementsByClassName("side_dropdown_btn");
 var i;
 
@@ -23,6 +22,33 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+// card open & close
+
+function myFunction() {
+    var x = document.getElementById("cli");
+    if (x.innerHTML === "Click Me") {
+        x.innerHTML = "HIHIHI";
+    } else {
+        x.innerHTML = "Click Me";
+    }
+}
+
+var acc = document.getElementsByClassName("top");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var bottom = this.nextElementSibling;
+    if (bottom.style.maxHeight){
+      bottom.style.maxHeight = null;
+    } else {
+      bottom.style.maxHeight = bottom.scrollHeight + "px";
     }
   });
 }
