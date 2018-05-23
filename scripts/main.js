2133f9008c58c4941292b968b31284e1204ce5ee
JerryLee <leechiyin1990@gmail.com>
2018-05-23 23:38:29 +0930

    remove useless js

--- scripts/main.js
+++ scripts/main.js
@@ -11,20 +11,7 @@ function mobilenav_close(){
   document.getElementById("sidemenu").style.width = "0";
 }
 
-var dropdown = document.getElementsByClassName("side_dropdown_btn");
-var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
 
 
 // card open & close
